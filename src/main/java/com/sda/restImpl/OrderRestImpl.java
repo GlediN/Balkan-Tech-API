3a288002558e9f76ed20e1c801a9dcef201314d9
Gledi <gledinezha1@gmail.com>
2023-11-13 19:23:18 +0100

    Handling bug thats not saving orders to database

--- src/main/java/com/sda/restImpl/OrderRestImpl.java
+++ src/main/java/com/sda/restImpl/OrderRestImpl.java
@@ -1,5 +1,6 @@
 package com.sda.restImpl;
 
+import com.sda.dao.OrderDao;
 import com.sda.rest.OrderRest;
 import com.sda.service.OrderService;
 import com.sda.serviceImpl.OrderServiceImpl;
@@ -15,17 +16,15 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
-@Getter
-@Setter
 public class OrderRestImpl implements OrderRest {
     final OrderService orderService;
+    final OrderDao orderDao;
     @Override
     public ResponseEntity<String> checkout(Map<String, String> requestMap) {
         try {
-            return orderService.getOrder(requestMap);
+            return orderService.saveOrder(requestMap);
         } catch (Exception e) {
             e.printStackTrace();
-
         }
         return HelpfulUtils.getResponseEntity(HelpfulUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
--- src/main/java/com/sda/service/OrderService.java
+++ src/main/java/com/sda/service/OrderService.java
@@ -6,4 +6,5 @@ import java.util.Map;
 
 public interface OrderService {
     ResponseEntity<String> getOrder(Map<String,String> requestMap);
+    ResponseEntity<String> saveOrder(Map<String,String> requestMap);
 }
